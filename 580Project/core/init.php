d15722b3eae1004d52c5d99606a39dc18e4b1898
cbergey1 <colin.bergey@gmail.com>
2017-04-17 11:26:32 -0400

    Add files via upload

--- 580Project/core/init.php
+++ 580Project/core/init.php
@@ -5,6 +5,8 @@ error_reporting(1);
 require 'database/connect.php';
 require 'phpfunctions/general.php';
 require 'phpfunctions/users.php';
+require 'phpfunctions/teacher.php';
+require 'phpfunctions/student.php';
 
 
 ?>
